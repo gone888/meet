634cdd43fcf21ccdf849f5fcb659d8d2c153d3ea
gone888 <bryanboyle.mail@gmail.com>
2025-10-11 05:46:43 -0400

    APM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import * as atatus from "atatus-spa";
+
+atatus.config("e59dc2c44b3a40e797784b1a09e2457e").install();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+atatus.notify(new Error("Test Atatus Setup"));
