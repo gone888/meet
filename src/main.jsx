2266bee96de127b19c8b9bddc9630f87888b112b
gone888 <bryanboyle.mail@gmail.com>
2025-10-12 08:58:39 -0400

    PWA update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import * as atatus from "atatus-spa";
-atatus.config("141c6b0ce4344d44aace75760bd30bbe").install();
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+// import * as atatus from "atatus-spa";
+// atatus.config("141c6b0ce4344d44aace75760bd30bbe").install();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+serviceWorkerRegistration.register();
